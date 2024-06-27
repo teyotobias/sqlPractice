bf4456737f675672d7e63dd218276309ba217c91
teyotobias <teyotobias@utexas.edu>
2024-06-27 15:54:49 -0500

    Minor changes to avoid duplication.

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,14 +1,16 @@
 -- SERIAL, which means it will automatically generate a unique, sequential integer for each new row inserted into the table. 
 -- The PRIMARY KEY constraint ensures that the id column is unique for each row and is used as the primary key for the table.
 
-CREATE TABLE customers (
+/*  if statements for duplicate entries */
+
+CREATE TABLE IF NOT EXISTS customers (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100),
     email VARCHAR(100)
 );
 
 
-CREATE TABLE orders (
+CREATE TABLE IF NOT EXISTS orders (
     id SERIAL PRIMARY KEY,
     customer_id INT,
     amount DECIMAL(10,2),
--- scripts/insert_data.sql
+++ scripts/insert_data.sql
@@ -1,7 +1,15 @@
 -- INSERT sample data
 
-INSERT INTO customers (name, email) VALUES ('Teyo', 'teyotobias@utexas.edu');
-INSERT INTO customers(name, email) VALUES ('Paula', 'paulaturrubiates@hotmail.com');
+-- ON CONFLICT : in case of duplicate entries.
 
-INSERT INTO orders(customer_id, amount, order_date) VALUES(8, 100.00, '2024-06-01');
-INSERT INTO orders(customer_id, amount, order_date) VALUES (9, 150.00, '2024-06-10');
+INSERT INTO customers (name, email) VALUES ('Teyo', 'teyotobias@utexas.edu')
+ON CONFLICT (email) DO NOTHING;
+
+INSERT INTO customers(name, email) VALUES ('Paula', 'paulaturrubiates@hotmail.com')
+ON CONFLICT (email) DO NOTHING;
+
+INSERT INTO orders(customer_id, amount, order_date) VALUES(8, 100.00, '2024-06-01')
+ON CONFLICT (customer_id, order_date) DO NOTHING;
+
+INSERT INTO orders(customer_id, amount, order_date) VALUES (9, 150.00, '2024-06-10')
+ON CONFLICT (customer_id, order_date) DO NOTHING;
