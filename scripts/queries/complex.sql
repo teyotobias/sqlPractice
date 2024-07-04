477b68c7456cc638a05d14807a5a63fda75742f6
teyotobias <teyotobias@utexas.edu>
2024-07-04 17:56:11 -0500

    Practicing complex queries with WITH AS common table expressions (CTE)

--- scripts/queries/complex.sql
+++ scripts/queries/complex.sql
@@ -48,3 +48,67 @@ SELECT month,
        (total_revenue - LAG(total_revenue) OVER (ORDER BY month)) / LAG(total_revenue) OVER (ORDER BY month) * 100 AS growth_rate
 FROM monthly_revenue
 ORDER BY month;
+
+
+-- Common Table Expressions (CTEs)
+
+WITH LastOrder AS (
+    SELECT customer_id, MAX(order_date) AS last_order_date
+    FROM orders
+    GROUP BY customer_id
+)
+SELECT customers.name, LastOrder.last_order_date
+FROM customers
+JOIN LastOrder ON customer.id = LastOrder.customer_id;
+
+
+-- Top N Customers by Total Spending
+
+WITH TotalSpending AS (
+    SELECT customer_id, SUM(amount) AS total_spent
+    FROM orders
+    GROUP BY customer_id
+)
+SELECT customers.name, TotalSpending.total_spent
+FROM customers
+JOIN TotalSpending ON customers.id = TotalSpending.customer_id
+ORDER BY TotalSpending.total_spent DESC
+LIMIT 5;
+
+
+/*
+What does LAG do?
+LAG is used to access daa from a previous row in the same result set without using a self-join.
+It is particularly useful for comparing values between rows and is commonly used in time
+series analysis, financial calculations, and trend analysis.
+
+LAG (scalar_expression [,offset] [,default]) OVER ([PARTITION BY partition_expression] ORDER BY order_expression)
+1. scalar_expression: The value to be returned from a previous row.
+2. offset (optional): The number of rows back 
+3. default (optional): The value to return if the offset goes beyond the result set. The default is 'NULL'.
+
+Usage: The 'LAG' function is used within the 'OVER' clause, which defines the window (set of rows) over which the function operates.
+
+Basic Usage: to compare each row's value with the previou row's value
+SELECT order_date, amount, LAG(amount) OVER (ORDER BY order_date) AS previous_amount
+FROM orders;
+
+The above query returns the order date, the current amount, and the amount from the previous row.
+If there is no previous row, it returns 0.
+*/
+
+-- Monthly Revenue Growth Rate
+WITH MonthlyRevenue AS (
+    SELECT Date_TRUNC('month', order_date) AS month, SUM(amount) AS total_revenue
+    FROM orders
+    GROUP BY month
+),
+RevenueGrowth AS (
+    SELECT month, total_revenue, 
+    LAG(total_revenue) OVER (ORDER BY month) AS previous_month_revenue,
+    (total_revenue - LAG(total_revenue) OVER (ORDER by month)) / LAG(total_revenue) OVER (ORDER BY month) * 100 AS growth_rate
+FROM MonthlyRevenue
+)
+SELECT month, total_revenue, growth_rate
+FROM RevenueGrowth
+ORDER BY month;
